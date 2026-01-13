5b90d65f78f77d50e2e6f17a79a37743891e7e21
jtithy <tithyjebunnesa202@gmail.com>
2026-01-13 22:03:33 +0600

    Implement java for run different datatypes

---
+++ Literals.java
@@ -0,0 +1,18 @@
+public class Literals {
+    
+    public static void main(String[] args) {
+        byte age = 70;
+        char ch = 'A';
+        float f1 = 5.6f;
+        double d1 = 4.660D;
+        long ageDino = 56666666666677978L;
+        String str = "Tithy";
+        System.out.println(age);
+        System.out.println(ch);
+        System.out.println(f1);
+        System.out.println(d1);
+        System.out.println(ageDino);
+        System.out.println(str);
+    }
+
+}
