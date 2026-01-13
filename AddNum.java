7248e0dcadc80096a4d40e1cbdbd5acf942ea1bd
jtithy <tithyjebunnesa202@gmail.com>
2026-01-13 22:02:17 +0600

    Implementing java for addding numbers

---
+++ AddNum.java
@@ -0,0 +1,9 @@
+public class AddNum {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        int num3 = num1 + num2;
+        System.out.print("The sum is:");
+        System.out.print(num3);
+    }
+}
