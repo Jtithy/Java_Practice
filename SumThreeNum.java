b95736e97b8b73a6bfff62bcf9e9f6c471428820
jtithy <tithyjebunnesa202@gmail.com>
2026-01-14 08:03:16 +0600

    Add SumThreeNum class to calculate and display the sum of three numbers

---
+++ SumThreeNum.java
@@ -0,0 +1,12 @@
+
+public class SumThreeNum {
+
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        int num3 = 50;
+        int sum = num1 + num2 + num3;
+        System.out.print("The sum is:");
+        System.out.print(sum);
+    }
+}
