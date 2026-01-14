5774bf17cdd950f23169c3ce9bb7382441f20f42
jtithy <tithyjebunnesa202@gmail.com>
2026-01-14 22:05:57 +0600

    Add Strings class to demonstrate string initialization and output in Java

---
+++ Strings.java
@@ -0,0 +1,10 @@
+
+public class Strings {
+
+    public static void main(String[] args) {
+        String name1 = new String("Tithy");
+        String name2 = "Tahmid";
+        System.out.println("Name1: " + name1);
+        System.out.println("Name2:" + name2);
+    }
+}
