c5840bdcd9ad072845510d99ea98f8e518c7af03
jtithy <tithyjebunnesa202@gmail.com>
2026-01-14 20:05:14 +0600

    Add OperatorPrecedencs class to demonstrate operator precedence in Java

---
+++ OperatorPrecedencs.java
@@ -0,0 +1,10 @@
+
+public class OperatorPrecedencs {
+
+    public static void main(String[] args) {
+        int a = 6 + 3 * 5 - 10 / 2;
+        int b = 6 - 3 / 5 + 10 * 2;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
