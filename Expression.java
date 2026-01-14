8f0547a617959ef39b189634899b8f4934d5a689
jtithy <tithyjebunnesa202@gmail.com>
2026-01-14 22:00:31 +0600

    Add Expression class to demonstrate arithmetic operations and their results in Java

---
+++ Expression.java
@@ -0,0 +1,9 @@
+// What will be the result of the following expression float a = 7 / 4 * 9 / 2; in Java?
+
+public class Expression {
+
+    public static void main(String[] args) {
+        float a = 7 / 4 * 9 / 2;
+        System.out.println(a);
+    }
+}
