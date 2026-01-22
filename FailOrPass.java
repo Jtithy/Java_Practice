498db5011650e48bbf913e7bdaa4df6ad0cc1135
jtithy <tithyjebunnesa202@gmail.com>
2026-01-22 19:24:58 +0600

    Implement a program to check pass or not

--- FailOrPass.java
+++ FailOrPass.java
@@ -8,13 +8,15 @@ public class FailOrPass {
 
     public static void main(String[] args) {
         float m1, m2, m3;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your marks in Physics:");
-        m1 = sc.nextFloat();
-        System.out.println("Enter your marks in Chemistry:");
-        m2 = sc.nextFloat();
-        System.out.println("Enter your marks in Mathematics:");
-        m3 = sc.nextFloat();
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter your marks in Physics:");
+            m1 = sc.nextFloat();
+            System.out.println("Enter your marks in Chemistry:");
+            m2 = sc.nextFloat();
+            System.out.println("Enter your marks in Mathematics:");
+            m3 = sc.nextFloat();
+        }
+
         float avg = (m1 + m2 + m3) / 3.0f;
         if (avg >= 40 && m1 >= 33 && m2 >= 33 && m3 >= 33) {
             System.out.println("Congratulation. You're passed");
