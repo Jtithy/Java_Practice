a97b434784e991a163d038a7cca33c51676a218a
jtithy <tithyjebunnesa202@gmail.com>
2026-01-23 12:19:05 +0600

    Printing even in decreament order

--- Even_Numbers.java
+++ Even_Numbers.java
@@ -2,13 +2,13 @@
 public class Even_Numbers {
 
     public static void main(String[] args) {
-        System.out.println("Printing even numbers:");
-        int i = 1;
-        while (i <= 50) {
+        System.out.println("Printing even numbers in decreament order:");
+        int i = 50;
+        while (i >= 1) {
             if (i % 2 == 0) {
                 System.out.println(i);
             }
-            i++;
+            i--;
         }
     }
 }
