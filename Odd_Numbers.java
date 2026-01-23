cdfd65487ccb7eb86b4aeb6630aa1d8073de6694
jtithy <tithyjebunnesa202@gmail.com>
2026-01-23 12:14:15 +0600

    Printing odd numbers only in java

---
+++ Odd_Numbers.java
@@ -0,0 +1,13 @@
+
+public class Odd_Numbers {
+
+    public static void main(String[] args) {
+        System.out.println("Printing odd numbers only:");
+        int i;
+        for (i = 0; i <= 50; i++) {
+            if (i % 2 == 1) {
+                System.out.println(i);
+            }
+        }
+    }
+}
