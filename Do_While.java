b4bcb22223504258ffd5ebbe873ca7430e53c960
jtithy <tithyjebunnesa202@gmail.com>
2026-01-23 12:08:32 +0600

    Do while implementation

---
+++ Do_While.java
@@ -0,0 +1,13 @@
+
+public class Do_While {
+
+    public static void main(String[] args) {
+        System.out.println("Using Do While loop:");
+        int i = 100;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i <= 150);
+    }
+
+}
