02335c1f990d077cd67e362ddc0d52cc98ac2c62
jtithy <tithyjebunnesa202@gmail.com>
2026-01-23 12:31:34 +0600

    Game rules set

--- RockPaperScissor.java
+++ RockPaperScissor.java
@@ -5,11 +5,28 @@ import java.util.Scanner;
 public class RockPaperScissor {
 
     public static void main(String[] args) {
-        String choice;
-        try (Scanner write = new Scanner(System.in)) {
-            System.out.println("Enter your choice(Rock. Paper or Scissor): ");
-            choice = write.nextLine();
-        }
+        int choiceA = 0;
+        int choiceB = 0;
+        System.out.println("Choices: \n 1.Rock \n 2. Paper \n 3. Scissor");
+        try (Scanner input = new Scanner(System.in)) {
+            // 1st Choice
+            System.out.println("Enter your choice one(1, 2, 3): ");
+            if (choiceA >= 1 && choiceA <= 3) {
+                choiceA = input.nextInt();
+            } else {
+                System.out.println("Invalid choice.");
+            }
 
+            // 2nd Choice
+            System.out.println("Enter your choice two(1, 2, 3): ");
+            if (choiceB >= 1 && choiceB <= 3) {
+                choiceB = input.nextInt();
+            } else {
+                System.out.println("Invalid choice.");
+            }
+        }
+        if (choiceA == 1 && choiceB == 2) {
+
+        }
     }
 }
