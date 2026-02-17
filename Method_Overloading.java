736845f9321d1e04777901e52e2f88a652d08151
jtithy <tithyjebunnesa202@gmail.com>
2026-02-17 17:10:40 +0600

    Refactor Method_Overloading class to streamline methods and remove unused code

--- Method_Overloading.java
+++ Method_Overloading.java
@@ -1,41 +1,21 @@
 
-import java.util.Scanner;
-
 public class Method_Overloading {
 
-    static void tellJoke() {
-        System.out.println("I invented a new word! Plagiarism!");
+    static void foo() {
+        System.out.println("Good Morning!");
     }
 
-    static void change(int[] arr) {
-        arr[0] = 100;
+    static void foo(String a) {
+        System.out.println("Good Morning, " + a);
     }
 
-    static int logic1(int x, int y) {
-        int z;
-        if (x > y) {
-            z = x + y;
-        } else {
-            z = (x + y) * 5;
-        }
-        return z;
+    static void foo(int a) {
+        System.out.println(a);
     }
 
     public static void main(String[] args) {
-        System.out.println("Enter the values: ");
-        int a;
-        int b;
-        try (Scanner sc = new Scanner(System.in)) {
-            a = sc.nextInt();
-            b = sc.nextInt();
-        }
-        int c;
-        c = logic1(a, b);
-        System.out.println(c);
-        int[] marks = {52, 73, 88, 91, 64};
-        change(marks);
-        //Reference is passed in change method
-        System.out.println("After changing: " + marks[0]);
-        tellJoke();
+        foo();
+        foo("World!");
+        foo(1000);
     }
 }
