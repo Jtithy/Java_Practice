30c4e982736bd4f2f15b29c35fed7c43fab405e1
jtithy <tithyjebunnesa202@gmail.com>
2026-02-17 18:06:07 +0600

    Add iterative factorial method to Recursion class

--- Recursion.java
+++ Recursion.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 public class Recursion {
 
+    //Method 1
     static int factorial(int n) {
         // fsctorial (n) = n*n-1*...*1
         if (n == 0 || n == 1) {
@@ -12,12 +13,28 @@ public class Recursion {
         }
     }
 
+    //Method 2
+    static int iterativeFactorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        } else {
+            int product = 1;
+            for (int i = 1; i <= n; i++) {
+                product *= i;
+            }
+            return product;
+        }
+
+    }
+
     public static void main(String[] args) {
         System.out.println("Enter the number: ");
         try (Scanner sc = new Scanner(System.in)) {
             int num = sc.nextInt();
             int result = factorial(num);
             System.out.println("Factorial of " + num + " is: " + result);
+            int iterativeResult = iterativeFactorial(num);
+            System.out.println("Iterative factorial of " + num + " is: " + iterativeResult);
         }
     }
 }
